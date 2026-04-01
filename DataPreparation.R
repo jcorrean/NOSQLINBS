064c676b02a707770ac9c54e723b2925c16acde1
Juan C. Correa <j.correa.n@gmail.com>
2026-04-01 04:10:40 -0600

    .

--- DataPreparation.R
+++ DataPreparation.R
@@ -1,3 +1,15 @@
+library(haven)
+
+speed_dating <- read_sav("Untitled/speed.dating.3lev.small.july25.2014.sav")
+
+# zap_labels() elimina etiquetas SPSS conservando los valores numéricos originales
+speed_dating <- as.data.frame(lapply(speed_dating, function(x) {
+        if (inherits(x, "haven_labelled")) zap_labels(x) else x
+}))
+
+write.csv(speed_dating, "speed_dating.csv",
+          row.names = FALSE, na = "")
+
 # Install the haven package if not already installed
 # install.packages("haven")
 
