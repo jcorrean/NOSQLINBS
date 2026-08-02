eb58188563bda277cca888fe435c3b96af9a5080
Juan C. Correa <j.correa.n@gmail.com>
2026-08-01 18:58:05 -0600

    Create Cross_Random_Effect_Model.R

---
+++ Cross_Random_Effect_Model.R
@@ -0,0 +1,31 @@
+library(lme4)
+library(lmerTest)
+dating_graph2 <- read.csv("query_export.csv")
+# Ensure separate factor spaces for rater and partner
+dating_graph2$participant <- factor(dating_graph2$participant)
+dating_graph2$partner     <- factor(dating_graph2$partner)
+# Grand-mean center predictors
+dating_graph2$initial_attraction_c <- scale(
+ dating_graph2$initial_attraction, center = TRUE, scale = FALSE)
+dating_graph2$week_c <- dating_graph2$week - 1
+# Crossed random effects: rater (participant) + partner
+model_crossed2 <- lmer(
+ romantic_interest ~
+  initial_attraction_c +
+  week_c +
+  sex +
+  initial_attraction_c:week_c +
+  (1 | participant) +
+  (1 | partner),
+ data    = dating_graph2,
+ REML    = TRUE,
+ control = lmerControl(optimizer = "bobyqa")
+)
+summary(model_crossed2)
+length(unique(paste(
+ dating_graph2$participant,
+ dating_graph2$partner
+)))
+nrow(dating_graph2)
+length(unique(dating_graph2$partner))
+length(unique(dating_graph2$participant))
